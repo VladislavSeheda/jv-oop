8cc50f7fb881c02d5a6502faa6d2bf98d806f619
Владіслав <vladislav.seheda@gmail.com>
2025-08-05 11:34:59 +0300

    changed systemoutprintln

--- src/main/java/core/basesyntax/Bulldozer.java
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -6,6 +6,6 @@ public class Bulldozer extends Machine {
     }
 
     public void stopWork() {
-        System.out.println("Bulldozer stopped work:");
+        System.out.println("Bulldozer stopped work");
     }
 }
--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -6,6 +6,6 @@ public class Excavator extends Machine {
     }
 
     public void stopWork() {
-        System.out.println("Excavator stopped work:");
+        System.out.println("Excavator stopped work");
     }
 }
